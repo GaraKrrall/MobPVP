f11480a06965ba5bb0dfa8fe0b579e4c50258120
GaraKrrall <akglsms@gmail.com>
2025-11-10 23:15:05 +0300

    3.2.3-pre12 repo

--- src/client/java/mc/garakrral/client/renderer/entity/model/SnotBallModel.java
+++ src/client/java/mc/garakrral/client/renderer/entity/model/SnotBallModel.java
@@ -1,12 +1,22 @@
 package mc.garakrral.client.renderer.entity.model;
 
-import net.minecraft.client.model.*;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 
 import mc.garakrral.entity.mob.SnotBallEntity;
 
+@Environment(EnvType.CLIENT)
 public class SnotBallModel extends EntityModel<SnotBallEntity> {
     private final ModelPart bb_main;
 
@@ -17,17 +27,19 @@ public class SnotBallModel extends EntityModel<SnotBallEntity> {
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-
-        modelPartData.addChild("bb_main", ModelPartBuilder.create()
-                        .uv(0, 0).cuboid(-7.0F, -10.0F, -7.0F, 14.0F, 2.0F, 14.0F)
-                        .uv(0, 16).cuboid(-6.0F, -12.0F, -6.0F, 12.0F, 2.0F, 12.0F)
-                        .uv(0, 30).cuboid(-5.0F, -14.0F, -5.0F, 10.0F, 2.0F, 10.0F)
-                        .uv(20, 0).cuboid(-4.0F, -16.0F, -4.0F, 8.0F, 2.0F, 8.0F)
-                        .uv(40, 0).cuboid(-3.0F, -18.0F, -3.0F, 6.0F, 2.0F, 6.0F),
-                ModelTransform.pivot(0.0F, 24.0F, 0.0F));
-
+        ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(-18, -12).cuboid(-7.0F, -10.0F, -7.0F, 14.0F, 2.0F, 14.0F, new Dilation(0.0F))
+                .uv(-6, -4).cuboid(-3.0F, -2.0F, -3.0F, 6.0F, 2.0F, 6.0F, new Dilation(0.0F))
+                .uv(-9, -6).cuboid(-4.0F, -4.0F, -4.0F, 8.0F, 2.0F, 8.0F, new Dilation(0.0F))
+                .uv(-12, -8).cuboid(-5.0F, -6.0F, -5.0F, 10.0F, 2.0F, 10.0F, new Dilation(0.0F))
+                .uv(-15, -10).cuboid(-6.0F, -12.0F, -6.0F, 12.0F, 2.0F, 12.0F, new Dilation(0.0F))
+                .uv(-15, -10).cuboid(-6.0F, -8.0F, -6.0F, 12.0F, 2.0F, 12.0F, new Dilation(0.0F))
+                .uv(-12, -8).cuboid(-5.0F, -14.0F, -5.0F, 10.0F, 2.0F, 10.0F, new Dilation(0.0F))
+                .uv(-9, -6).cuboid(-4.0F, -16.0F, -4.0F, 8.0F, 2.0F, 8.0F, new Dilation(0.0F))
+                .uv(-6, -4).cuboid(-3.0F, -18.0F, -3.0F, 6.0F, 2.0F, 6.0F, new Dilation(0.0F))
+                .uv(-6, -4).cuboid(-3.0F, -18.0F, -3.0F, 6.0F, 2.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 32);
     }
+
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int color) {
         bb_main.render(matrices, vertices, light, overlay);
--- src/main/java/mc/garakrral/entity/mob/SnotBallEntity.java
+++ src/main/java/mc/garakrral/entity/mob/SnotBallEntity.java
@@ -3,19 +3,125 @@ package mc.garakrral.entity.mob;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class SnotBallEntity extends SlimeEntity {
+
+    private static final TrackedData<Boolean> TAMED =
+            DataTracker.registerData(SnotBallEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
+    private boolean isCharging = false;
+
     public SnotBallEntity(EntityType<? extends SlimeEntity> type, World world) {
         super(type, world);
+        this.setSize(1, true);
     }
 
     public static DefaultAttributeContainer.Builder createAttributes() {
         return SlimeEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0D)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.55D)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0D);
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 20.0D)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.55D);
+    }
 
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(TAMED, false);
+    }
+
+    public boolean isTamed() {
+        return this.dataTracker.get(TAMED);
+    }
+
+    public void setTamed(boolean tamed) {
+        this.dataTracker.set(TAMED, tamed);
+    }
+
+    // Evcilleştirme ve binme
+    @Override
+    protected ActionResult interactMob(PlayerEntity player, Hand hand) {
+        if (!this.getWorld().isClient) {
+            if (!isTamed() && player.getStackInHand(hand).getItem() == Items.SLIME_BALL) {
+                this.setTamed(true);
+                player.getStackInHand(hand).decrement(1);
+                player.sendMessage(Text.literal("SnotBall seni sevdi 💚"), true);
+                return ActionResult.SUCCESS;
+            } else if (isTamed()) {
+                player.startRiding(this);
+                return ActionResult.SUCCESS;
+            }
+        }
+        return ActionResult.PASS;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.hasPassengers() && this.getFirstPassenger() instanceof PlayerEntity player) {
+
+            // Oyuncunun yönünü slime’a uygula
+            this.setYaw(player.getYaw());
+            this.prevYaw = this.getYaw();
+
+            // Zıplama kontrolü (client'ta değil, server tarafında güvenli)
+            if (player.isOnGround() && this.isOnGround()) {
+                // Oyuncu zıplamayı isterse (space basılıysa)
+                if (player.input != null && player.input.jumping) {
+                    isCharging = true;
+                } else if (isCharging) {
+                    // Tuş bırakıldığında zıpla
+                    isCharging = false;
+                    Vec3d look = player.getRotationVec(1.0F);
+                    double jumpPower = 0.8D;
+                    this.addVelocity(look.x * 0.8, jumpPower, look.z * 0.8);
+                    this.velocityDirty = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void remove(RemovalReason reason) {
+        super.remove(reason);
+    }
+
+    @Override
+    public void setSize(int size, boolean heal) {
+        super.setSize(1, heal);
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.setSize(1, false);
+        this.setTamed(nbt.getBoolean("Tamed"));
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("Tamed", this.isTamed());
+    }
+
+    @Override
+    public boolean collides() {
+        return true;
+    }
+
+    @Override
+    public boolean canBeControlledByRider() {
+        return true;
     }
 }
